9176d68af635cda43214310c42430540d4311b9f
Ruslan Purii <ruslanpurii@gmail.com>
2017-07-16 11:50:06 +0300

    mongodb init / film schema added

---
+++ controllers/filmapi.js
@@ -0,0 +1,21 @@
+var Thread = require('../models/thread.js');
+var Post = require('../models/post.js');
+var Film = require('../models/film');
+
+exports.show = function(req, res){
+    Film.findOne({id: req.params.id}, function(error, film) {
+        res.send(film);
+    });
+}
+
+exports.showAll = function(req, res){
+    Film.find(function(err, films) {
+        res.send(films);
+    });
+}
+
+exports.post = function(req, res) {
+    new Film({id: req.body.id, title: req.body.title, title_original: req.body.title_original,
+        year: req.body.year, country: req.body.country, genre: req.body.genre,
+        director: req.body.director, actors: req.body.actors, kp: req.body.kp, fb: 0}).save();
+}
---
+++ models/film.js
@@ -0,0 +1,20 @@
+// The Film model
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var filmSchema = new Schema({
+    id: Number,
+    title: String,
+    title_original: String,
+    year: ObjectId,
+    country: [ObjectId],
+    genre: [ObjectId],
+    director: [ObjectId],
+    actors: [ObjectId],
+    kp: Number,
+    fb: Number
+});
+
+module.exports = mongoose.model('Film', filmSchema);
