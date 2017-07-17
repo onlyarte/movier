d3b261d4c66b2b6523693f3466884a1ed94bf370
Ruslan Purii <ruslanpurii@gmail.com>
2017-07-17 21:31:18 +0300

    extended film info

--- routes/films.js
+++ routes/films.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
 var request = require('request-json');
 var client = request.createClient('http://getmovie.cc/');
 
-/* GET users listing. */
+/* GET film page. */
 router.get('/:filmid', function(req, res, next) {
     var path = 'api/kinopoisk.json?id=' + req.params.filmid + '&token=037313259a17be837be3bd04a51bf678';
     client.get(path, function(err, kpres, body) {
+        if(err){
+            return res.render('error', {error: err});
+        }
+
         var filmObj = JSON.parse(kpres.body);
-        res.render('film', { title: filmObj.name_en,
+        res.render('film', { title_original: filmObj.name_en,
+            title_rus: filmObj.name_ru,
             year: filmObj.year,
             country: filmObj.country,
             genre: filmObj.genre,
-            description: filmObj.description});
+            director: filmObj.creators.director,
+            description: filmObj.description,
+            poster: filmObj.poster_film_big,
+            rating_kp: filmObj.rating.kp_rating,
+            rating_imdb: filmObj.rating.imdb});
     });
 });
 
