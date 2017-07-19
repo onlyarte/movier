beb617f3fc4360b5c451aa4cf8f65091e5aec0d6
Ruslan Purii <ruslanpurii@gmail.com>
2017-07-19 17:23:29 +0300

    changed localStorage vars

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -7,7 +7,7 @@ function onSignIn(googleUser) {
         // save token to update user data on server if requested
         localStorage.setItem('id_token', id_token);
         // if user already authenticated do not notify server again
-        if(localStorage.auth && localStorage.id == profile.getId())
+        if(localStorage.auth && localStorage.user._id == profile.getId())
             return;
     }
     else
@@ -18,12 +18,16 @@ function onSignIn(googleUser) {
     xhr.open('POST', '/auth/' + id_token);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
-        console.log('Signed in as: ' + xhr.responseText);
-        console.log(xhr.responseText);
-        localStorage.setItem('auth', true);
-        localStorage.setItem('id', profile.getId());
-        localStorage.setItem('name', profile.getName());
-        localStorage.setItem('imageURL', profile.getImageUrl());
+        var user = JSON.parse(xhr.responseText);
+        if(user != null){
+            console.log('Signed in as: ' + user._id);
+            console.log(user);
+            localStorage.setItem('user', user);
+            localStorage.setItem('auth', true);
+            /*localStorage.setItem('id', profile.getId());
+            localStorage.setItem('name', profile.getName());
+            localStorage.setItem('imageURL', profile.getImageUrl());*/
+        }
     };
     xhr.send();
 }
