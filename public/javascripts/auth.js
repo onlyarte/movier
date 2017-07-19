931a44314528c3fabe16c531c4992515b30f91eb
Ruslan Purii <ruslanpurii@gmail.com>
2017-07-19 17:49:09 +0300

    heroku auth test #3

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,9 +1,32 @@
 function onSignIn(googleUser) {
+// Useful data for your client-side scripts:
+var profile = googleUser.getBasicProfile();
+
+// The ID token you need to pass to your backend:
+var id_token = googleUser.getAuthResponse().id_token;
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', '/auth/' + id_token);
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onload = function() {
+    console.log(xhr.responseText);
+    /*var user = JSON.parse(xhr.responseText);
+    if(user != null){
+        console.log('Signed in as: ' + user._id);
+        console.log(user);
+        localStorage.setItem('user', user);
+        localStorage.setItem('auth', true);
+    }*/
+};
+xhr.send(null);
+};
+
+/*function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
 
-    var id_token = googleUser.getAuthResponse().id_token;
+    //var id_token = googleUser.getAuthResponse().id_token;
     // if user authenticated
-    /*if(typeof(Storage) !== 'undefined'){
+    if(typeof(Storage) !== 'undefined'){
         // save token to update user data on server if requested
         localStorage.setItem('id_token', id_token);
         // if user already authenticated do not notify server again
@@ -11,7 +34,7 @@ function onSignIn(googleUser) {
             return;
     }
     else
-        return;*/
+        return;
 
     // if user not authenticated, send request to server
     var xhr = new XMLHttpRequest();
@@ -20,12 +43,12 @@ function onSignIn(googleUser) {
     xhr.onload = function() {
         console.log(xhr.responseText);
         //var user = JSON.parse(xhr.responseText);
-        /*if(user != null){
+        if(user != null){
             console.log('Signed in as: ' + user._id);
             console.log(user);
             localStorage.setItem('user', user);
             localStorage.setItem('auth', true);
-        }*/
+        }
     };
     xhr.send();
-}
+}*/
