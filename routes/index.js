5f1e3cd9b799c28ffc567027c2b267bb3281549f
Ruslan Purii <ruslanpurii@gmail.com>
2017-09-04 18:44:31 +0300

    hide save button if not logged in

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MOVIER' });
+  res.render('index', { title: 'MOVIER', channel: req.session.channel });
 });
 
 module.exports = router;
