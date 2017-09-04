c0a94e8441c172d0e35addf26f905749b223bc78
Ruslan Purii <ruslanpurii@gmail.com>
2017-09-04 18:35:34 +0300

    added session auth

--- routes/auth.js
+++ routes/auth.js
@@ -6,27 +6,14 @@ var cloudinary = require('cloudinary');
 
 //log in
 router.post('/', function(req, res, next) {
-    //Check that the fields are not empty
-    req.checkBody('login', 'Login required').notEmpty();
-    req.checkBody('password', 'Password required').notEmpty();
-
-    //Trim and escape the fields.
-    req.sanitize('login').escape(); req.sanitize('login').trim();
-
-    //Run the validators
-    var errors = req.validationErrors();
-    if(errors){
-        res.render('index', { title: 'MOVIER' });
-    } else {
-        channelapi.findById(req.params.login, function(error, channel){
-            if(error)
-                res.render('index', { title: 'MOVIER' });
-            else {
-                req.session.channel = channel;
-                res.redirect('/channel/' + channel._id);
-            }
-        });
-    }
+    channelapi.findById(req.params.login, function(error, channel){
+        if(error)
+            res.render('index', { title: 'MOVIER' });
+        else {
+            req.session.channel = channel;
+            res.redirect('/channel/' + channel._id);
+        }
+    });
 });
 
 //register
@@ -47,23 +34,25 @@ router.post('/new', function(req, res, next){
             }
 
             console.log('saved localy');
-            cloudinary.v2.uploader.upload(path, function(error, result) {
-                if(error)
-                    return next(error);
-                console.log('saved to cloud');
-                var newchannel = {
-                    id: req.body.login,
-                    email: req.body.email,
-                    password: req.body.password,
-                    name: req.body.password,
-                    image: result.url,
-                    lists: [],
-                    saved_lists: []
-                }
+            cloudinary.v2.uploader.upload(path,
+                {width: 1000, height: 1000, crop: "limit"},
+                function(error, result) {
+                    if(error)
+                        return next(error);
+                    console.log('saved to cloud');
+                    var newchannel = {
+                        id: req.body.login,
+                        email: req.body.email,
+                        password: req.body.password,
+                        name: req.body.password,
+                        image: result.url,
+                        lists: [],
+                        saved_lists: []
+                    }
 
-                channelapi.add(newchannel);
-                res.redirect('/channel/' + newchannel.id);
-            });
+                    channelapi.add(newchannel);
+                    res.redirect('/channel/' + newchannel.id);
+                });
         });
     });
 });
