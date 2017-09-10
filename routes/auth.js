45e0f175139d176557d70b35ab141e665951a2e1
Ruslan Purii <ruslanpurii@gmail.com>
2017-09-10 13:29:02 +0300

    fixed image saving after response

--- routes/auth.js
+++ routes/auth.js
@@ -20,11 +20,6 @@ router.post('/', function(req, res, next) {
 
 //register
 router.post('/new', function(req, res, next){
-    //check if exists
-    channelapi.findById(req.body.login, function(error, channel){
-        if(!error)
-            return next(new Error('Login already exists'));
-    });
 
     var channel = {
         id: req.body.login,
@@ -36,54 +31,79 @@ router.post('/new', function(req, res, next){
         saved_lists: []
     }
 
+    //check if exists
+    channelapi.findById(channel.id, function(error, channel){
+        if(!error)
+            return next(new Error('Login already exists'));
+
+        saveImage();
+    });
+
     //save image localy
-    var path = './public/images/temp/' + req.body.login + req.files.image.name;
-    var file = req.files.image;
-    file.mv(path, function(error) {
-        if (error)
-            return next(error);
-    });
-
-    //save iage to cloudinary
-    cloudinary.v2.uploader.upload(path,
-        { width: 1000, height: 1000,  crop: "limit" },
-        function(error, result) {
-            if(error)
+    function saveImageLocally(){
+        var path = './public/images/temp/' + channel.is + req.files.image.name;
+        var file = req.files.image;
+        file.mv(path, function(error) {
+            if (error)
                 return next(error);
 
-            channel.image = result.url;
-    });
+            saveImageToCloud();
+        });
+    }
 
-    //save channel to db
-    channelapi.add(channel, function(error, channel){
-        if(error)
-            return next(error);
-    });
-
-    //create and add defalt lists to channel
-    var addList = function(name){
-        listapi.add({
-            owner: channel.id,
-            is_open: false,
-            name: name,
-            films: []
-        }, function(error, list){
-            if(error)
-                return next(error);
-
-            channelapi.addList(channel.id, list._id, function(error, channel){
+    //save image to cloudinary
+    function saveImageToCloud(){
+        cloudinary.v2.uploader.upload(path,
+            { width: 1000, height: 1000,  crop: "limit" },
+            function(error, result) {
                 if(error)
                     return next(error);
-            });
-        });
-    };
-    addList('Любимые');
-    addList('Просмотренные');
-    addList('Буду смотреть');
 
-    //add session var
-    req.session.channel = channel.id;
-    res.redirect('/channel/' + channel.id);
+                channel.image = result.url;
+                saveChannel();
+        });
+    }
+
+    //save channel to db
+    function saveChannel(){
+        channelapi.add(channel, function(error, channel){
+            if(error)
+                return next(error);
+
+            addDefaultLists();
+        });
+    }
+
+    //create and add defalt lists to channel
+    function addDefaultLists(){
+        var addList = function(name){
+            listapi.add({
+                owner: channel.id,
+                is_open: false,
+                name: name,
+                films: []
+            }, function(error, list){
+                if(error)
+                    return next(error);
+
+                channelapi.addList(channel.id, list._id, function(error, channel){
+                    if(error)
+                        return next(error);
+                });
+            });
+        };
+        addList('Любимые');
+        addList('Просмотренные');
+        addList('Буду смотреть');
+
+        redirect();
+    }
+
+    //save session var and redirect to channel
+    function redirect(){
+        req.session.channel = channel.id;
+        res.redirect('/channel/' + channel.id);
+    }
 
 });
 
