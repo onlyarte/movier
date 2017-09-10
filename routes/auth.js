f9984fc1cd0f26e8ab032c95536d64ea30e03cac
Ruslan Purii <ruslanpurii@gmail.com>
2017-09-10 12:34:19 +0300

    fixed reg list linking

--- routes/auth.js
+++ routes/auth.js
@@ -20,64 +20,76 @@ router.post('/', function(req, res, next) {
 
 //register
 router.post('/new', function(req, res, next){
-
+    //check if exists
     channelapi.findById(req.body.login, function(error, channel){
         if(!error)
             return next(new Error('Login already exists'));
-
-        //save image localy
-        var path = './public/images/temp/' + req.body.login + req.files.image.name;
-        var file = req.files.image;
-        file.mv(path, function(error) {
-            console.log('tried to save localy');
-            if (error){
-                console.log(error);
-                return next(error);
-            }
-
-            console.log('saved localy');
-            cloudinary.v2.uploader.upload(path,
-                {width: 1000, height: 1000, crop: "limit"},
-                function(error, result) {
-                    if(error)
-                        return next(error);
-                    console.log('saved to cloud');
-                    var newchannel = {
-                        id: req.body.login,
-                        email: req.body.email,
-                        password: req.body.password,
-                        name: req.body.name,
-                        image: result.url,
-                        lists: [],
-                        saved_lists: []
-                    };
-
-                    channelapi.add(newchannel, function(error, added_channel){
-                        var addList = function(_name){
-                            listapi.add({
-                                owner: added_channel._id,
-                                is_open: false,
-                                name: _name,
-                                films: []
-                            }, function(error, list){
-                                if(!error)
-                                    added_channel._lists.push(list._id);
-                            });
-                        };
-
-                        addList('Любимые'); addList('Просмотренные'); addList('Буду смотреть');
-                        added_channel.save();
-                        channelapi.findById(added_channel._id, function(error, populated_channel){
-                            if(error)
-                                return next(error);
-
-                            req.session.channel = populated_channel;
-                            res.redirect('/channel/' + populated_channel._id);
-                        });
-                    });
-                });
-        });
     });
+
+    var channel = {
+        id: req.body.login,
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        image: null,
+        lists: [],
+        saved_lists: []
+    }
+
+    //save image localy
+    var path = './public/images/temp/' + req.body.login + req.files.image.name;
+    var file = req.files.image;
+    file.mv(path, function(error) {
+        if (error)
+            return next(error);
+    });
+
+    //save iage to cloudinary
+    cloudinary.v2.uploader.upload(path,
+        { width: 1000, height: 1000,  crop: "limit" },
+        function(error, result) {
+            if(error)
+                return next(error);
+
+            channel.image = result.url;
+    });
+
+    //save channel to db
+    channelapi.add(channel, function(error, channel){
+        if(error)
+            return next(error);
+    });
+
+    //create and add defalt lists to channel
+    var addList = function(name){
+        listapi.add({
+            owner: channel.id,
+            is_open: false,
+            name: name,
+            films: []
+        }, function(error, list){
+            if(error)
+                return next(error);
+
+            channelapi.addList(channel.id, list._id, function(error, channel){
+                if(error)
+                    return next(error);
+            });
+        });
+    };
+    addList('Любимые');
+    addList('Просмотренные');
+    addList('Буду смотреть');
+
+    //save channel as session variable
+    channelapi.findById(channel.id, function(error, populated_channel){
+        if(error)
+            return next(error);
+
+        req.session.channel = populated_channel;
+        res.redirect('/channel/' + populated_channel._id);
+    });
+
 });
 
 module.exports = router;
