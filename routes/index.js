a7451d101950f590c8b644f707a4cb0d57761b73
Ruslan Purii <ruslanpurii@gmail.com>
2017-10-06 14:14:05 +0300

    fixed index import

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const channelapi = require('../controllers/channelapi');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
