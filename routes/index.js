39be59c5363805f5e3ad3a0585750f5b042f7076
Ruslan Purii <ruslanpurii@gmail.com>
2017-10-06 13:52:10 +0300

    fixed index render

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
             res.render('index', { title: 'MOVIER', authch: channel });
         });
     } else {
-        res.render('film', { film : film });
+        res.render('index', { title: 'MOVIER' });
     }
 });
 
