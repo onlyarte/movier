a6bd5e254b5b980a7992ad6da0d6a3722d758ac2
Ruslan Purii <ruslanpurii@gmail.com>
2017-10-10 11:24:34 +0300

    set index coverlist

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const channelapi = require('../controllers/channelapi');
+const listapi = require('../controllers/listapi');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(req.session.channel){
         channelapi.findById(req.session.channel, function(error, channel){
             if(error)
-                next(error);
-            channel._password = null;
-            res.render('index', { title: 'MOVIER', authch: channel });
+                return next(error);
+
+            listapi.findById('59dc82011ed8420012bea72a', function(error, coverlist){
+                if(error)
+                return next(error);
+
+                channel._password = null;
+                res.render('index', { title: 'MOVIER', authch: channel, list: coverlist });
+            });
         });
     } else {
-        res.render('index', { title: 'MOVIER' });
+        listapi.findById('59dc82011ed8420012bea72a', function(error, coverlist){
+            if(error)
+            return next(error);
+
+            channel._password = null;
+            res.render('index', { title: 'MOVIER', list: coverlist });
+        });
     }
 });
 
