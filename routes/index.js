ba73c5bb139d836968e9e4affe7ffa188de63488
Ruslan Purii <ruslanpurii@gmail.com>
2018-01-03 17:17:07 +0200

    removed sensitive info

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const listapi = require('../controllers/listapi');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const coverId = '5a3f6a213ba5cf00142dab47';
+  const coverId = process.env.ind_cover || '5a3f6a213ba5cf00142dab47';
 
   if (req.session.login) {
     res.redirect(`/list/${coverId}`);
